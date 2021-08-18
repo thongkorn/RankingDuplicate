a5af824327bdce2ed88b37e21827a124cca031b9
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2021-08-18 15:50:16 +0700

    Add files via upload

---
+++ RankingDuplicate/RankingDuplicate/frmRankingDuplicate.designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmRankingDuplicate
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmRankingDuplicate))
+        Me.dgvData = New System.Windows.Forms.DataGridView()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btnProcess = New System.Windows.Forms.Button()
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgvData
+        '
+        Me.dgvData.AllowUserToAddRows = False
+        Me.dgvData.AllowUserToDeleteRows = False
+        Me.dgvData.AllowUserToOrderColumns = True
+        Me.dgvData.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.dgvData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvData.Location = New System.Drawing.Point(2, 2)
+        Me.dgvData.Name = "dgvData"
+        Me.dgvData.ReadOnly = True
+        Me.dgvData.RowHeadersVisible = False
+        Me.dgvData.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgvData.Size = New System.Drawing.Size(788, 428)
+        Me.dgvData.TabIndex = 1
+        '
+        'btnExit
+        '
+        Me.btnExit.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnExit.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnExit.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnExit.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.btnExit.Image = CType(resources.GetObject("btnExit.Image"), System.Drawing.Image)
+        Me.btnExit.Location = New System.Drawing.Point(687, 441)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(103, 35)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "   E&xit"
+        Me.btnExit.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'btnProcess
+        '
+        Me.btnProcess.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnProcess.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.btnProcess.Font = New System.Drawing.Font("Tahoma", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.btnProcess.ForeColor = System.Drawing.SystemColors.HotTrack
+        Me.btnProcess.Image = CType(resources.GetObject("btnProcess.Image"), System.Drawing.Image)
+        Me.btnProcess.Location = New System.Drawing.Point(577, 441)
+        Me.btnProcess.Name = "btnProcess"
+        Me.btnProcess.Size = New System.Drawing.Size(103, 35)
+        Me.btnProcess.TabIndex = 4
+        Me.btnProcess.Text = "Process"
+        Me.btnProcess.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.btnProcess.UseVisualStyleBackColor = True
+        '
+        'frmRankingDuplicate
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(793, 483)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnProcess)
+        Me.Controls.Add(Me.dgvData)
+        Me.Font = New System.Drawing.Font("Tahoma", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.Name = "frmRankingDuplicate"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ranking Duplicate Value - coDe bY: Thongkorn Tubtimkrob"
+        CType(Me.dgvData, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents dgvData As System.Windows.Forms.DataGridView
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents btnProcess As System.Windows.Forms.Button
+
+End Class
---
+++ RankingDuplicate/RankingDuplicate/frmRankingDuplicate.vb
@@ -0,0 +1,116 @@
+﻿' / ----------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More info: http://www.g2gnet.com/webboard
+' /
+' / Purpose: How to sort ranking duplicate in DataGridView without DataBase.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @CopyLeft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / ----------------------------------------------------------------
+Public Class frmRankingDuplicate
+    Dim dt As DataTable
+    Dim MaxRow As Integer = 15
+
+    ' / ----------------------------------------------------------------
+    '// Initialize DataGridView @Run Time
+    Private Sub InitGrid()
+        With dgvData
+            .RowHeadersVisible = False
+            .AllowUserToAddRows = False
+            .AllowUserToDeleteRows = False
+            .AllowUserToResizeRows = False
+            .MultiSelect = True
+            .SelectionMode = DataGridViewSelectionMode.FullRowSelect
+            .ReadOnly = True
+            .Font = New Font("Tahoma", 10)
+            ' Autosize Column
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            .AutoResizeColumns()
+            '// Even-Odd Color
+            .AlternatingRowsDefaultCellStyle.BackColor = Color.AliceBlue
+            ' Adjust Header Styles
+            With .ColumnHeadersDefaultCellStyle
+                .BackColor = Color.Navy
+                .ForeColor = Color.Black ' Color.White
+                .Font = New Font("Tahoma", 10, FontStyle.Bold)
+            End With
+        End With
+    End Sub
+
+    Private Sub frmRankingDuplicate_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        Application.Exit()
+    End Sub
+
+    ' / ----------------------------------------------------------------
+    Private Sub frmRankingDuplicate_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        Call InitGrid()
+        Call Ranking()
+    End Sub
+
+    ' / ----------------------------------------------------------------
+    Private Sub Ranking()
+        Dim ds As New DataSet()
+        dt = New DataTable
+        dt.Columns.Add("Student", GetType(String))
+        dt.Columns.Add("Subject A", GetType(Integer))
+        dt.Columns.Add("Subject B", GetType(Integer))
+        dt.Columns.Add("Subject C", GetType(Integer))
+        dt.Columns.Add("Average", GetType(Double))
+        dgvData.DataSource = dt
+        '//
+        Randomize()
+        Dim RandomClass As New Random()
+        For i As Integer = 0 To MaxRow - 1
+            Dim dr As DataRow = dt.NewRow()
+            '// RandomClass
+            dr(0) = "Student " & i + 1
+            dr(1) = RandomClass.Next(0, 100)
+            dr(2) = RandomClass.Next(0, 100)
+            dr(3) = RandomClass.Next(0, 100)
+            Dim SumCol As Double = 0
+            For iCol As Byte = 1 To 3 'MaxCol
+                SumCol = SumCol + dr(iCol)
+            Next
+            dr(4) = Format(SumCol / 3, "0.00")
+            '//
+            dt.Rows.Add(dr)
+        Next
+        '// Calcualte ranking
+        dgvData.DataSource = RankingDuplicate(dt, "Average")
+    End Sub
+
+    ' / ----------------------------------------------------------------
+    Public Function RankingDuplicate(dt As DataTable, fld As String) As DataTable
+        '// Descending (Z --> A)
+        Dim rankingDt = (From row In dt.AsEnumerable() Order By row.Field(Of Double)(fld) Descending.CopyToDataTable())
+        rankingDt.Columns.Add("Ranking")
+        Dim rank As Integer = 1
+        Dim count As Integer = 1
+        For i As Integer = 0 To rankingDt.Rows.Count - 2
+            rankingDt.Rows(i)("Ranking") = rank
+            '// If not duplicate value then increment +1.
+            If rankingDt.Rows(i)(fld).ToString() <> rankingDt.Rows(i + 1)(fld).ToString() Then
+                rank += 1
+                rank = count + 1
+            End If
+            count += 1
+        Next
+        rankingDt.Rows(rankingDt.Rows.Count - 1)("Ranking") = rank
+        Return rankingDt
+    End Function
+
+    Private Sub btnExit_Click(sender As System.Object, e As System.EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnProcess_Click(sender As System.Object, e As System.EventArgs) Handles btnProcess.Click
+        Call Ranking()
+    End Sub
+End Class
